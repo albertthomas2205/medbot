1f9c639c3feaa36f9334a13bbb5204e542438df4
albertthomas2205 <122583969+albertthomas2205@users.noreply.github.com>
2025-11-22 16:34:31 +0530

    update

--- asgi_uk_medical_bot-main/asgi_uk_medical_bot/settings.py
+++ asgi_uk_medical_bot-main/asgi_uk_medical_bot/settings.py
@@ -308,7 +308,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6380)],  # match your docker-compose service name + port
+            "hosts": [("127.0.0.1", 6379)],  # match your docker-compose service name + port
         },
     },
 }
